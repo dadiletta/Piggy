2ae468fb11a3fb066fdf4c85822d14be962c6871
Daniel Adiletta <dan.adiletta@gmail.com>
2019-09-25 08:57:28 -0400

    forming basic utilities

--- student.py
+++ student.py
@@ -2,15 +2,76 @@ from teacher import PiggyParent
 
 class Piggy(PiggyParent):
 
+    '''
+    *************
+    SYSTEM SETUP
+    *************
+    '''
+
     def __init__(self, *args, **kwargs):
         """Piggy constructor"""
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs) # this triggers the parent constructor
 
+        ''' 
+        MAGIC NUMBERS <-- where we hard-code our settings
+        '''
         self.LEFT_DEFAULT = 80
         self.RIGHT_DEFAULT = 80
+        self.MIDPOINT = 90  # what angle is straight forward for your bot?
 
     def load_defaults(self):
-        """Implements magic numbers defined in constructor"""
+        """Implements the magic numbers defined in constructor"""
+
+        self.set_motor_limits(self.MOTOR_LEFT, self.LEFT_DEFAULT)
+        self.set_motor_limits(self.MOTOR_RIGHT, self.RIGHT_DEFAULT)
         
-        self.set_motor_power(self.MOTOR_LEFT, self.LEFT_DEFAULT)
-        self.set_motor_power(self.MOTOR_RIGHT, self.RIGHT_DEFAULT)
+
+    def menu(self):
+        """Displays menu dictionary, takes key-input and calls method"""
+        ## This is a DICTIONARY, it's a list with custom index values. Python is cool.
+        # Please feel free to change the menu and add options. 
+        menu = {"n": ("Navigate", self.nav),
+                "d": ("Dance", self.dance),
+                "o": ("Obstacle count", self.obstacle_count),
+                "c": ("Calibrate", self.calibrate),
+                "q": ("Quit", self.quit)
+                }
+        # loop and print the menu...
+        for key in sorted(menu.keys()):
+            print(key + ":" + menu[key][0])
+        # store the user's answer
+        ans = input("Your selection: ")
+        # activate the item selected
+        menu.get(ans, [None, self.quit])[1]()
+
+    '''
+    ****************
+    STUDENT PROJECTS
+    ****************
+    '''
+
+    def dance(self):
+        print("I don't know how to dance. \nPlease give my programmer a zero.")
+
+    def obstacle_count(self):
+        print("I can't count how many obstacles are around me. Please give my programmer a zero.")
+
+    def nav(self):
+        print("-----------! NAVIGATION ACTIVATED !------------\n")
+        print("-------- [ Press CTRL + C to stop me ] --------\n")
+        print("-----------! NAVIGATION ACTIVATED !------------\n")
+        print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
+
+
+
+
+###########
+## MAIN APP
+if __name__ == "__main__":  # only run this loop if this is the main file
+    p = Piggy()
+    try:
+        while True:  # app loop
+            p.menu()
+
+    except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+        p.quit()  
--- teacher.py
+++ teacher.py
@@ -9,7 +9,27 @@
 # This code is an example for controlling the GoPiGo3 Motors
 #
 # Results:  When you run this program, manually rotate the left motor to control the speed of the right motor.
-import gopigo3
+import gopigo3, sys
 
 class PiggyParent(gopigo3.GoPiGo3):
-    pass
+    def __init__(self, addr=8, detect=True):
+        super().__init__(addr=addr, detect=detect)
+        if sys.version_info < (3, 0):
+            sys.stdout.write("Sorry, requires Python 3.x\n")
+            self.quit()
+
+    def deg_fwd(self, deg):
+        """Zeroes current encorder values then moves forward based on degrees given"""
+        self.offset_motor_encoder(self.MOTOR_LEFT, self.get_motor_encoder(self.MOTOR_LEFT))
+        self.offset_motor_encoder(self.MOTOR_RIGHT, self.get_motor_encoder(self.MOTOR_RIGHT))
+        self.set_motor_position(self.MOTOR_LEFT + self.MOTOR_RIGHT, deg)
+
+    def fwd(self):
+        self.set_motor_power(self.MOTOR_LEFT, self.LEFT_DEFAULT)
+        self.set_motor_power(self.MOTOR_RIGHT, self.RIGHT_DEFAULT)
+
+    def quit(self):
+        """Terminates robot movement and settings then closes app"""
+        print("\nIt's been a pleasure.\n")
+        self.reset_all()
+        sys.exit(1)
