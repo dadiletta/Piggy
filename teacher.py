b3958ccc384658d15f6d938ca30a224429fd4534
Daniel Adiletta <dan.adiletta@gmail.com>
2019-09-26 09:26:15 -0400

    fix fwd

--- teacher.py
+++ teacher.py
@@ -39,8 +39,12 @@ class PiggyParent(gopigo3.GoPiGo3):
         self.offset_motor_encoder(self.MOTOR_RIGHT, self.get_motor_encoder(self.MOTOR_RIGHT))
         self.set_motor_position(self.MOTOR_LEFT + self.MOTOR_RIGHT, deg)
 
-    def fwd(self, left=self.LEFT_DEFAULT, right=self.RIGHT_DEFAULT):
+    def fwd(self, left=50, right=50):
         """Blindly charges your robot forward at default power which needs to be configured in child class"""
+        if self.LEFT_DEFAULT and left == 50:
+            left = self.LEFT_DEFAULT
+        if self.RIGHT_DEFAULT and right == 50:
+            right = self.RIGHT_DEFAULT
         self.set_motor_power(self.MOTOR_LEFT, left)
         self.set_motor_power(self.MOTOR_RIGHT, right)
 
