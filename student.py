616e6cde9d70a95ea780c3d2c000e6f57ec8de0c
Daniel Adiletta <dan.adiletta@gmail.com>
2019-09-26 09:55:16 -0400

    added calibrate

--- student.py
+++ student.py
@@ -58,7 +58,7 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 3):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
 
--- teacher.py
+++ teacher.py
@@ -21,7 +21,44 @@ class PiggyParent(gopigo3.GoPiGo3):
         self.distance_sensor = EasyDistanceSensor()
 
     def calibrate(self):
-        pass
+        """allows user to experiment on finding centered midpoint and even motor speeds"""
+        print("Calibrating...")
+        self.servo(self.MIDPOINT)
+        response = str.lower(input("Am I looking straight ahead? (y/n): "))
+        if response == 'n':
+            while True:
+                response = str.lower(input("Turn right, left, or am I done? (r/l/d): "))
+                if response == "r":
+                    self.MIDPOINT += 25
+                    print("Midpoint: " + str(self.MIDPOINT))
+                    self.servo(self.MIDPOINT)
+                elif response == "l":
+                    self.MIDPOINT -= 25
+                    print("Midpoint: " + str(self.MIDPOINT))
+                    self.servo(self.MIDPOINT)
+                else:
+                    print("Midpoint now saved to: " + str(self.MIDPOINT))
+                    break
+        else:
+            print('Okay, remember %d as the correct self.MIDPOINT' % self.MIDPOINT)
+        response = str.lower(input("Do you want to check if I'm driving straight? (y/n)"))
+        if 'y' in response:
+            while True:
+                self.set_speed(self.LEFT_SPEED, self.RIGHT_SPEED)
+                self.fwd()
+                time.sleep(.5)
+                self.stop()
+                response = str.lower(input("Reduce left, reduce right or drive? (l/r/d): "))
+                if response == 'l':
+                    self.LEFT_SPEED -= 5
+                elif response == 'r':
+                    self.RIGHT_SPEED -= 5
+                elif response == 'd':
+                    self.fwd()
+                    time.sleep(.5)
+                    self.stop()
+                else:
+                    break
 
     def quit(self):
         """Terminates robot movement and settings then closes app"""
@@ -71,6 +108,11 @@ class PiggyParent(gopigo3.GoPiGo3):
         print("Servo moving to: {} ".format(angle))
         self.set_servo(self.SERVO_1, angle)
         time.sleep(.05)
+
+    def stop(self):
+        """Cut power to the motors"""
+        print("\n--STOPPING--\n")
+        self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
 
     '''
     SENSORS
