cb53d3e6fd6468893022c7fb480f3a3754fcaf1f
Daniel Adiletta <dan.adiletta@gmail.com>
2020-09-23 11:15:46 -0400

    test motion

--- student.py
+++ student.py
@@ -60,9 +60,10 @@ class Piggy(PiggyParent):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
         
-        for x in range(3):
-            self.shake()
-            # call other dance moves
+        # lower-ordered example...
+        self.fwd()
+        time.sleep(2)
+        self.stop()
 
     def safe_to_dance(self):
         """ Does a 360 distance check and returns true if safe """
